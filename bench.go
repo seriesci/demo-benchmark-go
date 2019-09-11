2bf0fbb00a5599a4ead7b8899366a7f1f5e0077a
Mirco Zeiss <mirco.zeiss@gmail.com>
2019-09-11 21:41:58 +0200

    sleep microseconds

--- bench.go
+++ bench.go
@@ -4,6 +4,6 @@ import "time"
 
 // Efficient is a super efficient function which needs a benchmark.
 func Efficient(a, b int) int {
-	time.Sleep(499 * time.Nanosecond)
+	time.Sleep(499 * time.Microsecond)
 	return a + b
 }
